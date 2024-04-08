0650b2d003d662a188d840f20bdacd4bf11d5b25
Alex Berrios <aberrios@grupoalto.com>
2024-04-08 17:23:05 -0400

    update

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,7 +5,7 @@ import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
     const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
-
+    //clave api: Op0gqvvRnB2MPIy2mwrIHV4BQMVVXmy5
     const onAddCategory = () => {
         setCategories([...categories, 'HunterXHunter']);
     }
